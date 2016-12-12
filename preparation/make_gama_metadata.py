27a8fbf8ebc4a49ac71e16ec2d8e3c0b90502fad
Steven Bamford <steven@stevenbamford.com>
2016-12-12 13:07:54 +0000

    Modify to produce JSON manifests

--- preparation/make_gama_metadata.py
+++ preparation/make_gama_metadata.py
@@ -4,21 +4,36 @@ from astropy.table import Table
 import numpy as np
 import warnings
 import os
+from collections import OrderedDict
+import json
 
-warnings.simplefilter("ignore", RuntimeWarning)
+# warnings.simplefilter("ignore", RuntimeWarning)
+
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(MyEncoder, self).default(obj)
+
 
 for region in ('09', '12', '15'):
-
+    manifest = []
     gama = Table.read('gamazoo{}.csv'.format(region))
-
+    survey = 'gama{}'.format(region)
     N = len(gama)
-
     t = Table()
 
     t['coords.0'] = gama['RA']
     t['coords.1'] = gama['DEC']
 
-    url_stub = "http://gzgama.s3.amazonaws.com/gamazoo{}".format(region)
+    #url_stub = "http://gzgama.s3.amazonaws.com/gamazoo{}".format(region)
+    url_stub = "http://s3.amazonaws.com/zooniverse-data/project_data/galaxy_zoo/gama{}".format(region)
 
     loc = [url_stub + '/native/' + x for x in gama['FILENAMENATIVE']]
     t['location.standard'] = loc
@@ -30,7 +45,7 @@ for region in ('09', '12', '15'):
            for x in gama['FILENAMENATIVE']]
     t['location.thumbnail'] = loc
 
-    t['metadata.survey'] = 'gama{}'.format(region)
+    t['metadata.survey'] = survey
 
     t['metadata.provided_image_id'] = gama['CATAID']
 
@@ -46,10 +61,23 @@ for region in ('09', '12', '15'):
 
     t['metadata.retire_at'] = 40
 
-    # Write to table
-    ftypes = ('csv','fits')
-    for ft in ftypes:
-        fname = '../manifests/gama{1}_metadata.{0}'.format(ft, region)
-        if os.path.isfile(fname):
-            os.remove(fname)
-        t.write(fname)
+    # Write to csv and fits tables
+    # ftypes = ('csv','fits')
+    # for ft in ftypes:
+    #     fname = '../manifests/gama{1}_metadata.{0}'.format(ft, region)
+    #     if os.path.isfile(fname):
+    #         os.remove(fname)
+    #     t.write(fname)
+
+    # Write as json
+    for row in t:
+        r = OrderedDict()
+        r['type'] = 'subject'
+        r['group_name'] = survey
+        r['group_type'] = 'survey'
+        for col in row.colnames:
+            r[col] = row[col]
+        manifest.append(r)
+    fname = '../manifests/{}_manifest.json'.format(survey)
+    with open(fname, 'w') as f:
+        json.dump(manifest, f, indent=0, separators=(',', ': '))
